68e76b1729f70bc76368d76727725e4bf4d7e32c
pla1n <danilkuisis@gmail.com>
2025-07-22 20:09:40 +0300

    d

--- src/components/Cards/CardList/CardList.jsx
+++ src/components/Cards/CardList/CardList.jsx
@@ -1,5 +1,7 @@
 import { CardItem } from "../CardItem/CardItem"
 import { List } from "./CardList.styled"
+import { useState, useEffect } from "react"
+import { fetchWeather, fetchLocation } from "components/services/weatherApi"
 
 export const CardList = () => {
     return (
--- src/components/services/weatherApi.js
+++ src/components/services/weatherApi.js
@@ -1,2 +1,14 @@
-const API_KEY = 'b46ebb01ba611714402a6d1507259a63'
-const BASE_URL = 'https://openweathermap.org'
+const API_KEY = 'b46ebb01ba611714402a6d1507259a63';
+const BASE_URL = 'https://api.openweathermap.org/data/3.0';
+
+export const fetchWeather = (lat, lon, part) => {
+  return fetch(
+    `${BASE_URL}/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${API_KEY}&units=metric`
+  ).then(response => response.json());
+};
+
+export const fetchLocation = city => {
+  return fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`
+  ).then(response => response.json());
+};
