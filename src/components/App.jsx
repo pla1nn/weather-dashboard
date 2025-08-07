f16ae4c2f7b11ab84719c005c45fc4a557af07bf
PanasenkoValeriy <124078834+PanasenkoValeriy@users.noreply.github.com>
2025-08-07 17:25:15 +0200

    gallery

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CardList } from './Cards/CardList/CardList';
 import Container from './Container/Container';
 import { Header } from './Header/Header';
@@ -7,51 +7,74 @@ import { SignUpForm } from './SignUpForm/SignUpForm';
 
 import { Hero } from './Hero/Hero';
 import { Pets } from './Pets/Pets';
-import { getLocation, getWeather, getHourlForecast } from '../services/weatherApi';
+import {
+  getLocation,
+  getWeather,
+  getHourlForecast,
+} from '../services/weatherApi';
 import { Stats } from './Stats/Stats';
-import { Table } from './Table/Table'
+import { Table } from './Table/Table';
 import { Graph } from './Graph/Graph';
+import Footer from './Footer/Footer';
+import Gallery from './Gallery/Gallery';
+import fetchImages from 'services/pixabayApi';
 
 export const App = () => {
   const [weather, setWeather] = useState(null);
-  const [forecast, setForecast] = useState(null)
+  const [forecast, setForecast] = useState(null);
   const [showSignUpForm, setShowSignUpForm] = useState(false);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const getImages = async () => {
+      try {
+        const data = await fetchImages();
+        setImages(data.hits);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getImages();
+  }, []);
 
   const handleSearch = cityName => {
     getLocation(cityName)
       .then(coords => {
-        getWeather(coords.lat, coords.lon).then(data => setWeather(data))
-        getHourlForecast(coords.lat, coords.lon).then(data => setForecast(data))
+        getWeather(coords.lat, coords.lon).then(data => setWeather(data));
+        getHourlForecast(coords.lat, coords.lon).then(data =>
+          setForecast(data)
+        );
       })
       .catch(() => {
-        setWeather(null)
-        setForecast(null)
-      }) 
-      
+        setWeather(null);
+        setForecast(null);
+      });
   };
 
   const openSignUpForm = () => setShowSignUpForm(true);
   const closeSignUpForm = () => setShowSignUpForm(false);
 
   return (
-  <>
-    <Container>
-      <Header onOpenSignUp={openSignUpForm} />
-    </Container>
+    <>
+      <Container>
+        <Header onOpenSignUp={openSignUpForm} />
+      </Container>
 
-    <Hero onSearch={handleSearch} />
+      <Hero onSearch={handleSearch} />
 
-    <Container>
-      {weather && <CardList weather={weather} />}
-      {weather && <Stats weather={weather} />}
-      {forecast && <Graph forecast={forecast} />}
-      {forecast && <Table forecast={forecast} />}
-      <Pets />
-    </Container>
+      <Container>
+        {weather && <CardList weather={weather} />}
+        {weather && <Stats weather={weather} />}
+        {forecast && <Graph forecast={forecast} />}
+        {forecast && <Table forecast={forecast} />}
+        <Pets />
+      </Container>
 
-    <MobileMenu onOpenSignUp={openSignUpForm} />
+      <MobileMenu onOpenSignUp={openSignUpForm} />
 
-    {showSignUpForm && <SignUpForm onClose={closeSignUpForm} />}
-  </>
+      {showSignUpForm && <SignUpForm onClose={closeSignUpForm} />}
+      <Gallery images={images} />
+      <Footer />
+    </>
   );
 };
